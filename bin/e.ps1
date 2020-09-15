dd1eec51435c6b40461d4af6c9f28eb97c0f7bb9
P D <partdavid@gmail.com>
2020-09-15 12:50:19 -0700

    Get rid of extraneous output related to searching for and not finding unix domain socket

--- bin/e.ps1
+++ bin/e.ps1
@@ -1,10 +1,17 @@
 #!/usr/bin/env pwsh
 
+$cmd = $Args
 
 if ($env:REMOTE_EMACS_CLIENT_NAME) {
-  $Args[-1] = "/ssh:$($env:REMOTE_EMACS_CLIENT_NAME):$(Join-Path -Path $PWD -ChildPath $Args[-1])"
+  $cmd[-1] = "/ssh:$($env:REMOTE_EMACS_CLIENT_NAME):$(Join-Path -Path $PWD -ChildPath $Args[-1])"
 }
 
-Write-Host (@($Args) -join ',')
+# I use TCP for the Emacs server, because I also use it remotely
+# from other machines
+if (Test-Path -Path "~/.emacs.d/server/server") {
+  $cmd += '-f','~/.emacs.d/server/server'
+}
 
-emacsclient @Args
+#Write-Host (@($Args) -join ',')
+
+emacsclient @cmd
