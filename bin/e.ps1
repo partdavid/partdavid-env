221f3eb163b1b657a927e877542eeeb4be7b3290
P D <partdavid@gmail.com>
2020-09-18 11:36:48 -0700

    Add better logic for making the parth

--- bin/e.ps1
+++ bin/e.ps1
@@ -3,7 +3,13 @@
 $cmd = $Args
 
 if ($env:REMOTE_EMACS_CLIENT_NAME) {
-  $cmd[-1] = "/ssh:$($env:REMOTE_EMACS_CLIENT_NAME):$(Join-Path -Path $PWD -ChildPath $Args[-1])"
+  $target = $cmd[-1]
+  if ([System.IO.Path]::IsPathRooted($target)) {
+    $target = "/ssh:$($env:REMOTE_EMACS_CLIENT_NAME):$target"
+  } else {
+    $target = "/ssh:$($env:REMOTE_EMACS_CLIENT_NAME):$(Join-Path -Path $PWD -ChildPath $target)"
+  }
+  $cmd[-1] = $target
 }
 
 # I use TCP for the Emacs server, because I also use it remotely
@@ -12,6 +18,6 @@ if (Test-Path -Path "~/.emacs.d/server/server") {
   $cmd += '-f','~/.emacs.d/server/server'
 }
 
-#Write-Host (@($Args) -join ',')
+Write-Host (@($cmd) -join ',')
 
 emacsclient @cmd
