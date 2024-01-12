5a7a39a833b17149029b4448df607264bba16faf
P D <partdavid@gmail.com>
2024-01-12 08:56:29 -0800

    Update 1Password-utilities to work with integrated auth

--- modules/1Password-Utilities/Start-1PasswordSession.ps1
+++ modules/1Password-Utilities/Start-1PasswordSession.ps1
@@ -4,6 +4,12 @@
 
 Start a 1Password session using the op command.
 
+.DESCRIPTION
+
+This command works with both manually-added accounts, as well
+as environments where you have enabled 1password app integration
+(where you signin by unlocking the app).
+
 #>
 function Start-1PasswordSession {
   [CmdletBinding(SupportsShouldProcess)]
@@ -13,13 +19,18 @@ function Start-1PasswordSession {
   )
 
   $spec = @{}
-  op account list --format json | ConvertFrom-Json | %{ $spec[$_.shorthand] = $_ }
+  op account list --format json | ConvertFrom-Json | %{
+    # Integrated CLI authentication doesn't use the 'shorthand'
+    # field, only manual accounts.
+    $shorthand = $_.shorthand ?? (($_.url -replace '\.1password\.com') -replace '-','_')
+    $spec[$shorthand] = $_
+  }
   if ($Accounts.Length -eq 0) {
     $Accounts = $spec.Keys
   }
   foreach ($account in $Accounts) {
     if ($PSCmdlet.ShouldProcess($account, "op signin")) {
-      op signin --account $account --raw | Set-Content Env:"OP_SESSION_$($spec[$account].user_uuid)"
+      op signin --account $spec[$account].user_uuid --raw | Set-Content Env:"OP_SESSION_$($spec[$account].user_uuid)"
     }
   }
 }
--- modules/1Password-Utilities/Stop-1PasswordSession.ps1
+++ modules/1Password-Utilities/Stop-1PasswordSession.ps1
@@ -12,13 +12,17 @@ function Stop-1PasswordSession {
   )
   $by_var = @{}
   $by_shorthand = @{}
-  op account list --format json | ConvertFrom-Json | `
-    %{ $by_var["OP_SESSION_$($_.user_uuid)"] = $_; $by_shorthand[$_.shorthand] = $_ }
+  op account list --format json | ConvertFrom-Json | %{
+    $shorthand = $_.shorthand ?? (($_.url -replace '\.1password\.com$') -replace '-','_')
+    $by_var["OP_SESSION_$($_.user_uuid)"] = $_
+    $by_shorthand[$shorthand] = $_
+  }
   if ($Accounts.Length -eq 0) {
     $Accounts = Get-ChildItem Env:OP_SESSION_* | %{ $by_var[$_.Name].shorthand ?? ($_.Name -replace '^OP_SESSION_')}
   }
   foreach ($account in $Accounts) {
     if ($PSCmdlet.ShouldProcess($account, 'sign out')) {
+      op signout --account $by_shorthand[$account].user_uuid
       Remove-Item Env:"OP_SESSION_$($by_shorthand[$account].user_uuid)" -Force -ErrorAction SilentlyContinue
       Remove-Item Env:"OP_SESSION_${account}" -Force -ErrorAction SilentlyContinue
     }
